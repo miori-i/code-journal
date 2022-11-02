d04d4ecc754a327f99d3049626514ce5f486b6b1
Miori Imai <imamio1227@gmail.com>
2022-11-02 15:02:19 +0000

    Listen for 'input' events on the photoUrl input to update the src attribute of the photo preview when the input value changes. #1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* global data */
+// /* global data */
 
 var $photoUrl = document.getElementById('photo-url');
 
@@ -11,30 +11,30 @@ $photoUrl.addEventListener('input', function (event) {
 
 });
 
-var $form = document.querySelector('form');
+// var $form = document.querySelector('form');
 
-$form.addEventListener('submit', function (event) {
+// $form.addEventListener('submit', function (event) {
 
-  // Put the form's input values into a new object.
-  var entry = {};
-  entry.title = $form.elements.title.value;
-  entry.photoUrl = $form.elements['photo-url'].value;
-  entry.notes = $form.elements.notes.value;
+//   // Put the form's input values into a new object.
+//   var entry = {};
+//   entry.title = $form.elements.title.value;
+//   entry.photoUrl = $form.elements['photo-url'].value;
+//   entry.notes = $form.elements.notes.value;
 
-  // Add the nextEntryId to the object.
-  // var nextEntryId = 0;
-  // entry.push(nextEntryId);
+//   // Add the nextEntryId to the object.
+//   // var nextEntryId = 0;
+//   // entry.push(nextEntryId);
 
-  // Increment the nextEntryId on the data model.
-  data.nextEntryId++;
+//   // Increment the nextEntryId on the data model.
+//   data.nextEntryId++;
 
-  // Prepend the new object to the entries in the data model.
-  data.entries.prepend(entry);
+//   // Prepend the new object to the entries in the data model.
+//   data.entries.prepend(entry);
 
-  // Reset the image preview's `src' attribute.
-  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+//   // Reset the image preview's `src' attribute.
+//   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
 
-  // Reset the form inputs.
-  $form.reset();
+//   // Reset the form inputs.
+//   $form.reset();
 
-});
+// });
