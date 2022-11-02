d729246596700236b1531d18ebdce67bf4f11784
Miori Imai <imamio1227@gmail.com>
2022-11-02 15:02:00 +0000

    Listen for 'input' events on the photoUrl input to update the src attribute of the photo preview when the input value changes. #1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,14 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+// To write the data in entries property before unload, stginfy it
+window.addEventListener('beforeunload', function (event) {
+  var entriesJSON = JSON.stringify(data.entries);
+  localStorage.setItem('javascript strage', entriesJSON);
+});
+
+var $entries = localStorage.getItem('javascript storage');
+if ($entries !== null) {
+  data.entries = JSON.parse($entries);
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-// /* global data */
+/* global data */
+
+var $photoUrl = document.getElementById('photo-url');
+
+var $img = document.getElementById('image');
+
+// Listen for 'input' events on the photoUrl input to update the src attribute
+// of the photo preview when the input value changes.
+$photoUrl.addEventListener('input', function (event) {
+  $img.setAttribute('src', $photoUrl.value);
+
+});
+
+var $form = document.querySelector('form');
+
+$form.addEventListener('submit', function (event) {
+
+  // Put the form's input values into a new object.
+  var entry = {};
+  entry.title = $form.elements.title.value;
+  entry.photoUrl = $form.elements['photo-url'].value;
+  entry.notes = $form.elements.notes.value;
+
+  // Add the nextEntryId to the object.
+  // var nextEntryId = 0;
+  // entry.push(nextEntryId);
+
+  // Increment the nextEntryId on the data model.
+  data.nextEntryId++;
+
+  // Prepend the new object to the entries in the data model.
+  data.entries.prepend(entry);
+
+  // Reset the image preview's `src' attribute.
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+
+  // Reset the form inputs.
+  $form.reset();
+
+});
