ab1b238104b867f46b18754aadc6d641f90d04c4
Miori Imai <imamio1227@gmail.com>
2022-11-02 17:01:37 +0000

    Listen for 'submit' events on the journal entry form #1

--- js/data.js
+++ js/data.js
@@ -9,11 +9,11 @@ var data = {
 
 // To write the data in entries property before unload, stginfy it
 window.addEventListener('beforeunload', function (event) {
-  var entriesJSON = JSON.stringify(data.entries);
+  var entriesJSON = JSON.stringify(data);
   localStorage.setItem('javascript strage', entriesJSON);
 });
 
 var $entries = localStorage.getItem('javascript storage');
 if ($entries !== null) {
-  data.entries = JSON.parse($entries);
+  data = JSON.parse($entries);
 }
